9f4e9b552eb544261024e5e8812e3edb961bbcb5
Maxim Kim <kimmaxim69@gmail.com>
2023-12-21 13:28:35 +0300

    Добавил файл locators. Провел рефакторинг кода для вынеса локаторов в отдельный файл

---
+++ locators/locators.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+class ProductPageLocators:
+    BUTTON_ADD_BASKET = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    NAME_PRODUCT = (By.XPATH, "//div[@class='col-sm-6 product_main']/h1")
+    BUTTON_ADD_REVIEW = (By.CSS_SELECTOR, "a[id='write_review']")
+    TITLE_REVIEW = (By.CSS_SELECTOR, "fieldset legend")
+    NAME_PRODUCT_BASKET = (By.XPATH, "//div[@class='col-sm-6 product_main']/h1")
+    ALERT_TEXT = (By.CSS_SELECTOR, "div[class='alertinner ']")
+    WISH_LIST = (By.XPATH, "//button[contains(@class, 'wishlist')]")
+    VIEW_BASKET = (By.CSS_SELECTOR, "a[class='btn btn-default']")
+    TITLE_BASKET = (By.XPATH, "//div[@class='page-header action']/h1")
+    BUTTON_CHECKOUT = (By.XPATH, "//a[contains(@class, 'btn-block')]")
+    TITLE_CHECKOUT = (By.XPATH, "//div[@class='sub-header']/h1")
--- tests/test_items.py
+++ tests/test_items.py
@@ -4,6 +4,8 @@ import random
 
 from selenium.webdriver.common.by import By
 
+from locators.locators import ProductPageLocators
+
 @allure.parent_suite("Интернет-магазин")
 @allure.suite("Книги")
 @allure.sub_suite("Страница продукта")
@@ -23,14 +25,14 @@ class TestProductPage:
         ]
         link = random.sample(list, k=1)
         browser.get(''.join(link))
-        assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket"), "The button busket have been not found"
+        assert browser.find_element(*ProductPageLocators.BUTTON_ADD_BASKET), "The button busket have been not found"
 
     @allure.severity(allure.severity_level.NORMAL)
     @allure.title("Проверка наличия наименования товара на странице")
     @pytest.mark.smoke
     def test_check_name_product(self, browser):
         """ Тест проверяет наличие и наименование продукта на странице """   
-        name_product = browser.find_element(By.XPATH, "//div[@class='col-sm-6 product_main']/h1").text
+        name_product = browser.find_element(*ProductPageLocators.NAME_PRODUCT).text
         assert len(name_product) > 0, "The page of product has been not loaded"
 
     @allure.severity(allure.severity_level.NORMAL)
@@ -38,9 +40,9 @@ class TestProductPage:
     @pytest.mark.smoke
     def test_write_review(self, browser):
         """ Тест проверяет возможность написания отзыва """
-        button_review = browser.find_element(By.CSS_SELECTOR, "a[id='write_review']")
+        button_review = browser.find_element(*ProductPageLocators.BUTTON_ADD_REVIEW)
         button_review.click()
-        title_review = browser.find_element(By.CSS_SELECTOR, "fieldset legend")
+        title_review = browser.find_element(*ProductPageLocators.TITLE_REVIEW)
         assert title_review is not None, "The button of review is not clickable"
 
     @allure.severity(allure.severity_level.BLOCKER)
@@ -48,10 +50,10 @@ class TestProductPage:
     @pytest.mark.smoke
     def test_add_product_in_basket(self, browser):
         """ Тест проверяет добавление товара в корзину """
-        button_basket = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+        button_basket = browser.find_element(*ProductPageLocators.BUTTON_ADD_BASKET)
         button_basket.click()
-        name_product = browser.find_element(By.XPATH, "//div[@class='col-sm-6 product_main']/h1").text
-        alert_text = browser.find_element(By.CSS_SELECTOR, "div[class='alertinner ']").text
+        name_product = browser.find_element(*ProductPageLocators.NAME_PRODUCT_BASKET).text
+        alert_text = browser.find_element(*ProductPageLocators.ALERT_TEXT).text
         assert name_product in alert_text, "The product has been not added"
 
     @allure.severity(allure.severity_level.BLOCKER)
@@ -59,7 +61,7 @@ class TestProductPage:
     @pytest.mark.xfail(reason = 'Кнопка не кликабельна')
     def test_add_to_wish_list(self, browser):
         """ Тест проверяет добавление товара в избранное """
-        wish_list = browser.find_element(By.XPATH, "//button[contains(@class, 'wishlist')]")
+        wish_list = browser.find_element(*ProductPageLocators.WISH_LIST)
         wish_list.click()
         assert True, "The button of add to wish list is not clickable"
 
@@ -68,9 +70,9 @@ class TestProductPage:
     @pytest.mark.smoke
     def test_open_backet(self, browser):
         """ Тест проверяет открытие корзины """
-        view_basket = browser.find_element(By.CSS_SELECTOR, "a[class='btn btn-default']")
+        view_basket = browser.find_element(*ProductPageLocators.VIEW_BASKET)
         view_basket.click()
-        title_busket = browser.find_element(By.XPATH, "//div[@class='page-header action']/h1").text
+        title_busket = browser.find_element(*ProductPageLocators.TITLE_BASKET).text
         assert len(title_busket) > 0, "The page has been not loaded"
     
     @allure.severity(allure.severity_level.BLOCKER)
@@ -78,7 +80,7 @@ class TestProductPage:
     @pytest.mark.smoke
     def test_proceed_to_checkout(self, browser):
         """ Тест проверяет переход к оформлению заказа """
-        proceed_to_checkout = browser.find_element(By.XPATH, "//a[contains(@class, 'btn-block')]")
+        proceed_to_checkout = browser.find_element(*ProductPageLocators.BUTTON_CHECKOUT)
         proceed_to_checkout.click()
-        title_checkout = browser.find_element(By.XPATH, "//div[@class='sub-header']/h1").text
+        title_checkout = browser.find_element(*ProductPageLocators.TITLE_CHECKOUT).text
         assert len(title_checkout) > 0, "The page has been not loaded"
